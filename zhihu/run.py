5a734f76500edac280aaa8e5d119ae6b377be143
Wang JL <jimmymall@live.com>
2022-06-27 16:55:10 +0800

    Only when date is even

--- zhihu/run.py
+++ zhihu/run.py
@@ -3,11 +3,13 @@ import random
 
 num=random.randint(0,9)
 flag=num>5
+date=int( time.strftime("%d", time.localtime()) )
 
 timsString=time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())
 print(timsString)
 
-if (flag):
+
+if (date % 2 == 0) and (flag):
 	print(flag)
 	fa = open(r"test.txt", "a")
 	fa.write( str(time.time()) +":\t"+ timsString + "\n" )

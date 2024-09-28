881953d7a037ddc3b974edba2534f9297ffcc008
Wang Junliang <jimmymall@live.com>
2024-09-28 15:27:27 +0800

    reduce chance

--- zhihu/run.py
+++ zhihu/run.py
@@ -2,7 +2,7 @@ import time
 import random
 
 num=random.randint(0,9)
-flag=num>5
+flag=num>7
 date=int( time.strftime("%d", time.localtime()) )
 
 timsString=time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())
